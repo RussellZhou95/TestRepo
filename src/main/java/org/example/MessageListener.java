02af2fc6e1209a7a244a3319968cf2421ca01ca2
RussellZhou <zhouy689@gmail.com>
2025-06-25 23:52:06 -0400

    Reapply "Adding second producer and listener"
    This reverts commit 8242f5bac778e40cae0b2c212ed3e0531a73d1e6.

--- src/main/java/org/example/MessageListener.java
+++ src/main/java/org/example/MessageListener.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import jakarta.jms.Message;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import jakarta.jms.JMSException;
+import jakarta.jms.TextMessage;
 
 @Component
 public class MessageListener {
@@ -9,5 +13,14 @@ public class MessageListener {
     @JmsListener(destination = "test-queue")
     public void receiveMessage(String message) {
         System.out.println("Received message: " + message);
+    }
+
+    @JmsListener(destination = "test-queue2")
+    public void receiveMessage2(Message msg) throws JMSException {
+        if(msg instanceof TextMessage ) {
+            TextMessage textMessage = (TextMessage) msg;
+            String stringMsg = textMessage.getText();
+            System.out.println("Received message: "+stringMsg);
+        }
     }
 }
